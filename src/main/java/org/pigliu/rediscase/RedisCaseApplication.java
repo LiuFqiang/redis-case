e692e6c8167edabe9a6763cc831a9580071f9709
liufuqiang <15641483049@163.com>
2022-11-17 18:36:50 +0800

    init

---
+++ src/main/java/org/pigliu/rediscase/RedisCaseApplication.java
@@ -0,0 +1,13 @@
+package org.pigliu.rediscase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCaseApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(RedisCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pigliu/rediscase/RedisCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pigliu.rediscase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisCaseApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
